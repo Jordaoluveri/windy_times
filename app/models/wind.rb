63a2183a4578d06df0490b958fc275ed27905a37
Jordao Ribeiro <jordaoluveri@gmail.com>
2015-07-06 09:36:32 -0300

    Some corrections

--- app/models/wind.rb
+++ app/models/wind.rb
@@ -7,9 +7,9 @@ class Wind
 		@wind = get_wind_speed
 	end
 
-	def get_wind_speed(lat, long)
+	def get_wind_speed
 		hours = Array.new
-		stream = open('https://api.forecast.io/forecast/f297e79d7626fa09115292bb0cd39f61/37.8267,-122.423').read
+		stream = open('https://api.forecast.io/forecast/f297e79d7626fa09115292bb0cd39f61/-22.999555,-43.348580').read
 		@return = JSON.parse(stream)
 		@return['hourly']['data'].each_with_index do |data, i|
 			time = Time.at(data['time'])
