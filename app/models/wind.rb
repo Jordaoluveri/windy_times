c9b00b7f286f0e6855a2b0dd42646123666c1f1f
Jordao Ribeiro <jordaoluveri@gmail.com>
2015-07-07 01:13:59 -0300

    Minor changes in default location

--- app/models/wind.rb
+++ app/models/wind.rb
@@ -3,13 +3,15 @@ require 'json'
 class Wind
 	attr_accessor :wind
 
-	def initialize
-		@wind = get_wind_speed
+	def initialize(lat = -22.999555, lng = -43.348580)
+		@lat = lat
+		@lng = lng
+		@wind = get_wind_speed(lat, lng)
 	end
 
-	def get_wind_speed
+	def get_wind_speed(lat, lng)
 		hours = Array.new
-		stream = open('https://api.forecast.io/forecast/f297e79d7626fa09115292bb0cd39f61/-22.999555,-43.348580').read
+		stream = open("https://api.forecast.io/forecast/f297e79d7626fa09115292bb0cd39f61/#{lat},#{lng}").read
 		@return = JSON.parse(stream)
 		@return['hourly']['data'].each_with_index do |data, i|
 			time = Time.at(data['time'])
